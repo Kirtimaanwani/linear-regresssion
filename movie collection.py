6352a51fa7c5b42983e22b5778c0d2ee4ff54a24
Kirtimaanwani <sushilsw111@gmail.com>
2022-08-22 12:53:54 +0530

    Update movie collection.py

--- movie collection.py
+++ movie collection.py
@@ -2,4 +2,10 @@ import pandas as pd
 collection = pd.read_csv("movie_collection.csv", index_col=0)
 print(collection.columns)
 
+# Well, you have successfully treated data
+# and it is ready to be used to train the model.
+#
+# Here's your eighth task of the project: Find the relationship
+# between budget of the movie and the amount
+# it will gross at the box office (collection).
 
